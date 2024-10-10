abd3466978f9b615ee8ec2da207b7bf5bf04e64a
Jakub Majewski <jakub.majewski@applover.pl>
2024-10-11 00:18:31 +0200

    Added Detection Screenshot Taken Functionality With Screen Share

--- EnvoySDK/Networking/CreateLink/CreateLinkRequest.swift
+++ EnvoySDK/Networking/CreateLink/CreateLinkRequest.swift
@@ -1,5 +1,5 @@
 import Foundation
-
+// base64_file: str
 public struct CreateLinkRequest: Codable {
     let autoplay: Bool?
     let contentSetting: ContentSetting
@@ -56,7 +56,8 @@ public struct CreateLinkRequest: Codable {
         let contentType: String
         let contentName: String
         let contentDescription: String
-        let common: Common
+        let common: Common?
+        let base64_file: String?
         let timeLimit: Int?
         let timeStart: Int?
         let availableFrom: String?
@@ -70,7 +71,7 @@ public struct CreateLinkRequest: Codable {
         let buttonText: String?
         let contentProtection: ContentProtection?
 
-        public init(contentType: String, contentName: String, contentDescription: String, common: Common,
+        public init(contentType: String, contentName: String, contentDescription: String, common: Common? = nil, base64_file: String? = nil,
                     timeLimit: Int? = nil, timeStart: Int? = nil, availableFrom: String? = nil, availableTo: String? = nil,
                     videoOrientation: VideoOrientation? = nil, previewTitle: String? = nil, previewDescription: String? = nil,
                     previewImage: String? = nil, mandatoryEmail: Bool? = nil,
@@ -79,6 +80,7 @@ public struct CreateLinkRequest: Codable {
             self.contentName = contentName
             self.contentDescription = contentDescription
             self.common = common
+            self.base64_file = base64_file
             self.timeLimit = timeLimit
             self.timeStart = timeStart
             self.availableFrom = availableFrom
